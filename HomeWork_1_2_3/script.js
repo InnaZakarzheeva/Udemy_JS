34ffcd492a490a731770428ca1e6bcf982ea74fb
InnaZakarzheieva <44522230+InnaZakarzheeva@users.noreply.github.com>
2019-07-26 11:03:38 +0300

    Add files via upload

---
+++ HomeWork_1_2_3/script.js
@@ -0,0 +1,120 @@
+'use strict';
+let money, time;
+
+function start() {
+    money = +prompt('Ваш бюджет на месяц?', '');
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt('Ваш бюджет на месяц?', '');
+    }
+}
+start();
+
+var appData = {
+    bank: money,
+    timeData: time,
+    expenses: {},
+    optionalExpense: {},
+    income: [],
+    savings: true,
+    chooseExpenses: function() {
+        for (let i = 0; i < 2; i++){
+            let  a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+                 b = prompt('Во сколько обойдется?', '');
+            if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50){
+                appData.expenses[a] = b;
+            } else {
+                i--;
+            }
+            
+        };
+    },
+    detectDayBudget: function() {
+        appData.moneyPerDay = (appData.bank / 30).toFixed();
+        alert ('Бюджет на 1 день составляет ' + appData.moneyPerDay);
+    },
+    detectLevel: function() {
+        if(appData.moneyPerDay < 100){
+            console.log('Минимальный уровень достатка');
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+            console.log('Средний уровень достатка');
+        } else if (appData.moneyPerDay > 2000){
+            console.log('Высокий уровень достатка');
+        } else {
+            console.log('Произошла ошибка');
+        }
+    },
+    checkSavings: function() {
+        if (appData.savings == true) {
+            let save = +prompt('Какова сумма накопления?'),
+                percent = +prompt('Под какой процент?');
+    
+            appData.monthIncome = save / 100 / 12 * percent;
+            alert('Доход в месяц с вашего депозита: ' + appData.monthIncome);
+        }
+    },
+    chooseOptExpenses: function() {
+        for (let i = 1; i <= 3; i++){
+            let question = prompt('Статья необязательных расходов');
+            appData.optionalExpense[i] = question;
+        }
+    },
+    chooseIncome: function() {
+        let items = prompt('Что принесет дополнительный доход? (Перечислите через запятую)', '');
+        while(!isNaN(items) || items== "" || items == null){
+            items = prompt('Что принесет дополнительный доход? (Перечислите через запятую)', '');
+        }
+        appData.income = items.split(', ');
+        appData.income.push(prompt('Может что-то еще?', ''));
+        appData.income.sort();
+        appData.income.forEach(function(index, element) {
+            alert('Способы доп. заработка: ' + index+1 + ':' + element)
+        })
+    },
+    enterAllData: function() {
+        for (var key in appData) {
+            if( appData.hasOwnProperty(key) ) {
+                alert('Наша программа включает в себя данные: ' + key + " = " + appData[key]);
+            }
+        } 
+    }
+}
+
+
+// let dedline = '2019-08-25';
+
+// function getTimeRemaining(endtime) {
+//     let t = Date.parse(endtime) - Date.parse(new Date()),
+//     seconds = Math.floor((t/1000) % 60),
+//     minutes = Math.floor((t/1000/60) % 60),
+//     hours = Math.floor((t/(1000*60*60)));
+
+//     return {
+//         'total': t,
+//         'hours': hours,
+//         'minutes': minutes,
+//         'seconds': seconds
+//     };
+// }
+
+// function setClock(id, endtime){
+//     let timer = document.getElementById(id),
+//     hours = document.getElementsByClassName('hours'),
+//     minutes = document.querySelector('.minutes'),
+//     seconds = document.querySelector('.seconds'),
+//     timeInterval = setInterval(updateClock, 1000);
+
+//     function updateClock() {
+//         let t = getTimeRemaining(endtime);
+//         hours.textContent = t.hours;
+//         minutes.textContent = t.minutes;
+//         seconds.textContent = t.seconds;
+
+//         if (t.total <= 0) {
+//             clearInterval(timeInterval)
+//         }
+//     }
+// }
+
+// setClock('timer', dedline);
