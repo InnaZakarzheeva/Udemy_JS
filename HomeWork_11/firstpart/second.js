5c68b03da1607da76fe481780c10a7da1f24d69b
InnaZakarzheieva <44522230+InnaZakarzheeva@users.noreply.github.com>
2019-07-26 12:14:24 +0300

    Delete second.js

--- HomeWork_11/firstpart/second.js
+++
@@ -1,6 +0,0 @@
-<input id="age" value="30"/>
-let age = document.getElementById('age');
-function showUser(surname, name) {
-    alert("Пользователь " + surname + " " + name + ", его возраст " + age.value);
-}
-showUser();
